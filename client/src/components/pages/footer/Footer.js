a90fce556c8cc6a92c3e14c41265b198b468a970
Patricia de Figueiredo Bettio <pfb@siteimprove.com>
2022-05-28 17:54:19 +0200

    Style footer

--- client/src/components/pages/footer/Footer.js
+++ client/src/components/pages/footer/Footer.js
@@ -1,82 +1,51 @@
 import React from "react";
 import "./Footer.css";
-import { Button } from "../../Button";
 import { Link } from "react-router-dom";
-import {
-  FaFacebook,
-  FaInstagram,
-  FaYoutube,
-  FaTwitter,
-  FaLinkedin,
-} from "react-icons/fa";
-import { MdFingerprint } from "react-icons/md";
+import { BsFacebook, BsInstagram, BsYoutube } from "react-icons/bs";
 
 function Footer() {
   return (
     <div className="footer-container">
-      <section className="footer-subscription">
-        <p className="footer-subscription-heading">
-          Join our exclusive membership to receive the latest news and trends
-        </p>
-        <p className="footer-subscription-text">
-          You can unsubscribe at any time.
-        </p>
-        <div className="input-areas">
-          <form>
-            <input
-              className="footer-input"
-              name="email"
-              type="email"
-              placeholder="Your Email"
-            />
-            <Button buttonStyle="btn--outline">Subscribe</Button>
-          </form>
-        </div>
-      </section>
-      <div className="footer-links">
-        <div className="footer-link-wrapper">
+      <div className="footer-info">
+        <div>
           <div className="footer-link-items">
-            <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
-          </div>
-          <div className="footer-link-items">
-            <h2>Contact Us</h2>
-            <Link to="/">Contact</Link>
-            <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
-            <Link to="/">Sponsorships</Link>
+            <h3>ABI | Rosa Gomes</h3>
+            <p>Contribuite to keep the project alive</p>
           </div>
         </div>
-        <div className="footer-link-wrapper">
+
+        <div className="footer-links-wrapper">
           <div className="footer-link-items">
-            <h2>Videos</h2>
-            <Link to="/">Submit Video</Link>
-            <Link to="/">Ambassadors</Link>
-            <Link to="/">Agency</Link>
-            <Link to="/">Influencer</Link>
+            <h3>Our partners</h3>
+            <div>
+              <Link to="/">
+                <img className="footer-logo" src="./images/cwbFC.png" />
+              </Link>
+              <Link to="/">
+                <img className="footer-logo" src="./images/prefeitura.png" />
+              </Link>
+            </div>
           </div>
+
           <div className="footer-link-items">
-            <h2>Social Media</h2>
-            <Link to="/">Instagram</Link>
-            <Link to="/">Facebook</Link>
-            <Link to="/">Youtube</Link>
-            <Link to="/">Twitter</Link>
+            <h3>Contact us</h3>
+            <p>Phone: 4545454545</p>
+            <p>email@email.com</p>
+          </div>
+
+          <div className="footer-link-items">
+            <h3>Find us</h3>
+            <p>Address street 45</p>
+            <p>Curitiba/PR</p>
+            <Link to="/">Map</Link>
           </div>
         </div>
       </div>
+
       <section className="social-media">
         <div className="social-media-wrap">
-          <div className="footer-logo">
-            <Link to="/" className="social-logo">
-              <MdFingerprint className="navbar-icon" />
-              LAVISH
-            </Link>
-          </div>
-          <small className="website-rights">LAVISH © 2020</small>
+          <small className="website-rights">PFB., © 2021</small>
+
           <div className="social-icons">
             <Link
               className="social-icon-link"
@@ -84,7 +53,7 @@ function Footer() {
               target="_blank"
               aria-label="Facebook"
             >
-              <FaFacebook />
+              <BsFacebook />
             </Link>
             <Link
               className="social-icon-link"
@@ -92,33 +61,15 @@ function Footer() {
               target="_blank"
               aria-label="Instagram"
             >
-              <FaInstagram />
+              <BsInstagram />
             </Link>
             <Link
               className="social-icon-link"
-              to={
-                "//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber"
-              }
+              to={"/"}
               target="_blank"
               aria-label="Youtube"
             >
-              <FaYoutube />
-            </Link>
-            <Link
-              className="social-icon-link"
-              to="/"
-              target="_blank"
-              aria-label="Twitter"
-            >
-              <FaTwitter />
-            </Link>
-            <Link
-              className="social-icon-link"
-              to="/"
-              target="_blank"
-              aria-label="LinkedIn"
-            >
-              <FaLinkedin />
+              <BsYoutube />
             </Link>
           </div>
         </div>
