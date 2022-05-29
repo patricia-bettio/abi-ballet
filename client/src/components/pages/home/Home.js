1a71687201ab451fa7dd331b2f7ffe33bfc43f84
Patricia de Figueiredo Bettio <pfb@siteimprove.com>
2022-05-29 16:08:38 +0200

    Add styling and apply color palette

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -1,25 +1,30 @@
 import React from "react";
 // import { TopGrid } from "../../Dividers";
-import { Section, SectionTest, SectionTest2 } from "../../Section";
+import {
+  SectionText,
+  SectionVideo,
+  SectionTest2,
+  SectionTest3,
+} from "../../Section";
 import { homeObjTwo, homeObjSix } from "./Data";
 // import { Gallery } from "../../Gallery";
-// import Gallery from "../../Gallery";
-import ContactForm from "../../ContactForm";
+import { Gallery } from "../../Gallery";
 // import Background from '../../../../public/images'
 
 function Home() {
   return (
     <>
       {/* <TopGrid /> */}
-      <SectionTest name={"Pat"} {...homeObjSix} />
-      <Section {...homeObjTwo} />
+      <SectionVideo name={"Pat"} {...homeObjSix} />
+      <SectionText {...homeObjTwo} />
       <SectionTest2 withForm />
+      <Gallery />
+      <SectionTest3 withForm />
       {/* <div>
         <Section {...homeObjTwo} />
         <NewForm label={"name"} />
       </div> */}
       {/* <Gallery /> */}
-      <ContactForm />
       {/* <Section withForm label={"Phone"} {...homeObjOne} />
       <Section {...homeObjThree} /> */}
     </>
