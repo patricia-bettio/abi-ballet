7c4ca47e05c5a852d987dc7d139f829195f84f35
Patricia de Figueiredo Bettio <pfb@siteimprove.com>
2022-05-30 22:44:21 +0200

    Add basic gallery

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -5,7 +5,7 @@ class Gallery extends React.Component {
     return (
       <div className="galleryWrapper">
         <h1>Gallery</h1>
-        <div className="col">
+        {/* <div classNameName="col">
           <iframe
             // width="560"
             // height="315"
@@ -14,6 +14,83 @@ class Gallery extends React.Component {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
+        </div> */}
+
+        <div className="container">
+          <div className="image-gallery">
+            <div className="column">
+              <div className="image-item">
+                <img
+                  src="https://pbstyledk.files.wordpress.com/2022/05/noticias.jpg"
+                  alt=""
+                />
+                <div className="overlay">
+                  <span>Image title</span>
+                </div>
+              </div>
+              <div className="image-item">
+                <img
+                  src="https://pbstyledk.files.wordpress.com/2022/05/rosa-aluna-1.jpg"
+                  alt=""
+                />
+                <div className="overlay">
+                  <span>Image title</span>
+                </div>
+              </div>
+              <div className="image-item">
+                <iframe
+                  width="100%"
+                  height="auto"
+                  src="https://www.youtube.com/embed/abp0mfFDfWM?controls=0"
+                  title="YouTube video player"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+            <div className="column">
+              {" "}
+              <div className="image-item">
+                <img
+                  src="https://pbstyledk.files.wordpress.com/2022/05/grupoazul.jpg"
+                  alt=""
+                />
+                <div className="overlay">
+                  <span>Image title</span>
+                </div>
+              </div>
+              <div className="image-item">
+                <img
+                  src="https://pbstyledk.files.wordpress.com/2022/05/aluna.jpg"
+                  alt=""
+                />
+                <div className="overlay">
+                  <span>Image title</span>
+                </div>
+              </div>
+            </div>
+            <div className="column">
+              {" "}
+              <div className="image-item">
+                <img
+                  src="https://pbstyledk.files.wordpress.com/2022/05/noticias.jpg"
+                  alt=""
+                />
+                <div className="overlay">
+                  <span>Image title</span>
+                </div>
+              </div>
+              <div className="image-item">
+                <img
+                  src="https://pbstyledk.files.wordpress.com/2022/05/turma-adulto.jpg"
+                  alt=""
+                />
+                <div className="overlay">
+                  <span>Image title</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
