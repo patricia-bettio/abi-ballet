91d0eaabfef024f540a0902144ef98f99212f1b1
Patricia de Figueiredo Bettio <pfb@siteimprove.com>
2022-06-13 10:44:18 +0200

    Cleanup app

--- client/src/App.js
+++ client/src/App.js
@@ -9,26 +9,8 @@ import {
   Route,
   Routes,
   Navigate,
-  // Link
 } from "react-router-dom";
 import Home from "./components/pages/home/Home";
-// import { render } from '../api/app';
-
-// class App extends React.Component {
-//   constructor(props){
-//     super(props);
-//     this.state = { apiResponse:""};
-//   }
-
-//   callAPI(){
-//     fetch('http://localhost:9000/testAPI')
-//     .then(res => res.text())
-//     .then(res => this.setState({apiResponse: res}));
-//   }
-
-//   componentWillMount(){
-//     this.callAPI();
-//   }
 
 class App extends React.Component {
   render() {
@@ -37,18 +19,7 @@ class App extends React.Component {
         <Menu />
         <div className="body-wrapper">
           <Routes>
-            {/* <Route path='/' exact component={Home}/> */}
             <Route path="/" element={<Home />} />
-            {/* {console.log("localS", localStorage)}
-            {console.log(
-              localStorage.getItem("user") === process.env.REACT_APP_ADMIN_USER
-            )} */}
-
-            {/* {localStorage &&
-              localStorage.getItem("user") ===
-                process.env.REACT_APP_ADMIN_USER && (
-                <Route path="/Admin" element={<Admin />} />
-              )} */}
             <Route
               path="/Admin"
               element={
@@ -62,17 +33,8 @@ class App extends React.Component {
               }
             />
             <Route path="/Login" element={<LogIn />} />
-            {/* <Route path="/">
-            <Home />
-          </Route> */}
           </Routes>
         </div>
-        {/* <Route path="/about">
-            <About />
-            </Route>
-            <Route path="/users">
-            <Users />
-          </Route> */}
         <Footer />
       </Router>
     );
